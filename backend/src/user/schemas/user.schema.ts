128ee386ed45d3e0cb0cf065aa5d884f751fe99d
Tanapong Chulwong <earthmagic2@hotmail.com>
2021-06-07 10:57:04 +0700

    initiate user src code

---
+++ backend/src/user/schemas/user.schema.ts
@@ -0,0 +1,9 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {}
+
+export const CatSchema = SchemaFactory.createForClass(User);
