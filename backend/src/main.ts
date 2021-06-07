2cb95aab613e6797f7ed1a2669d93d4048895a57
Tanapong Chulwong <earthmagic2@hotmail.com>
2021-06-07 18:21:41 +0700

    fixed cors bug and enable fastifyHelemt

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,13 +6,14 @@ import {
 } from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { fastifyHelmet } from 'fastify-helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-
+  await app.register(fastifyHelmet);
   const config = new DocumentBuilder()
     .setTitle('Practice Project')
     .setDescription('The practice project API description')
@@ -21,13 +22,11 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.enableCors();
   app.enableCors({
     origin: ['http://localhost:3000', process.env.URL],
     credentials: true,
   });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(8080, '127.0.0.1');
 }
 bootstrap();
