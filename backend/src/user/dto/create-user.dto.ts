eadbdc5953674ec23f4fd314287c63c9d2f758b0
Tanapong Chulwong <earthmagic2@hotmail.com>
2021-06-09 13:23:47 +0700

    fix api doc bug

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -6,35 +6,29 @@ import {
   IsOptional,
   IsString,
 } from 'class-validator';
-import { Role } from 'src/role/role.enum';
+import { Role } from '../../role/role.enum';
 
 export class CreateUserDto {
-  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   readonly firstName: string;
 
-  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   readonly lastName: string;
 
-  @ApiProperty()
   @IsNotEmpty()
   @IsNumberString()
   readonly mobile: string;
 
-  @ApiProperty()
   @IsNotEmpty()
   @IsEmail()
   readonly email: string;
 
-  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   readonly password: string;
 
-  @ApiProperty()
   @IsOptional()
   @IsString()
   role?: Role[];
--- backend/src/user/dto/update-user.dto.ts
+++ backend/src/user/dto/update-user.dto.ts
@@ -9,32 +9,26 @@ import {
 import { Role } from 'src/role/role.enum';
 
 export class UpdateUserDto {
-  @ApiProperty()
   @IsNotEmpty()
   @IsString()
-  id: String;
+  id: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   readonly firstName?: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   readonly lastName?: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsNumberString()
   readonly mobile?: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsEmail()
   readonly email?: string;
 
-  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   readonly role?: Role[];
